cbc75e9d857883dbc935e1afe612f2e33cfce63c
Tim Brady <timbrady@Tims-MacBook-Pro.local>
2020-09-15 16:29:43 -0600

    following class pattern

--- services/date.js
+++ services/date.js
@@ -2,18 +2,20 @@ const dateFnsTimeZone = require('date-fns-timezone');
 const dateFns = require('date-fns');
 
 class DateService {
-    timeZone = 'America/Denver';
-    pattern = 'D.M.YYYY HH:mm:ss.SSS';
-
-    getCurrentEpochDate = () => {
-        return new Date();
+    constructor() {
+        this.timeZone = 'America/Denver';
+        this.pattern = 'D.M.YYYY HH:mm:ss.SSS';
     }
 
-    convertTimeToHumanReadable = (time) => {
+    getCurrentEpochDate() {
+        return new Date();
+    };
+
+    convertTimeToHumanReadable(time) {
         return dateFnsTimeZone.formatToTimeZone(time, this.pattern, { timeZone: this.timeZone });
     };
 
-    timeBetween = (oldTime) => {
+    timeBetween(oldTime) {
         const test = dateFns.intervalToDuration({start: oldTime, end: this.getCurrentEpochDate()});
         console.log(test);
         return test;
